3ced3e87f77ed4bc209c0e6c899cd4fff96b2d8a
vova chacov <vvvoha13@gmail.com>
2019-10-01 23:05:39 +0300

    0.1

--- AlgoritmsLab3/Program.cs
+++ AlgoritmsLab3/Program.cs
@@ -10,6 +10,28 @@ namespace AlgoritmsLab3
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Stack<string> stack = new Stack<string>(8);
+                // добавляем четыре элемента
+                stack.Push("Kate");
+                stack.Push("Sam");
+                stack.Push("Alice");
+                stack.Push("Tom");
+                stack.ChangePlaces();
+                // извлекаем один элемент
+                var head = stack.Pop();
+                Console.WriteLine(head);    
+
+                // просто получаем верхушку стека без извлечения
+                head = stack.Peek();
+                Console.WriteLine(head);    
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ AlgoritmsLab3/Stack.cs
@@ -0,0 +1,72 @@
+﻿//1.Реалізувати стек.Для виконання операцій з елементами заданої структури створити функції, які додатково реалізують:
+//-основні операції над статичним/динамічним стеком;
+//-змінити місцями перший та останній елемент стеку;
+//-розвернути стек(зробити «дно» стеку вершиною, а вершину –«дном»);
+//-видалити всі елементи стеку;
+//-операція «чи належить заданий елемент» статичному/динамічному стеку.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsLab3
+{
+    class Stack<T>
+    {
+        private T[] items;
+        private int count;
+        const int n = 10;
+        public Stack()
+        {
+            items = new T[n];
+        }
+        public Stack(int length)
+        {
+            items = new T[length];
+        }
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+        public void Push(T item)
+        {
+            if (count == items.Length)
+                Resize(items.Length + 10);
+            items[count++] = item;
+        }
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is empty");
+            T item = items[--count];
+            items[count] = default(T);
+            if (count > 0 && count < items.Length - 10)
+                Resize(items.Length - 10);
+            return item;
+        }
+        public T Peek()
+        {
+            return items[count - 1];
+        }
+        private void Resize(int max)
+        {
+            T[] temp = new T[max];
+            for (int i = 0; i < count; i++)
+                temp[i] = items[i];
+            items = temp;
+        }
+        public void ChangePlaces()
+        {
+            T temp = default;
+            temp = items[0];
+            items[0] = items[count - 1];
+            items[count - 1] = temp;
+        }
+    }
+}
